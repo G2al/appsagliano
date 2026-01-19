e6774df3aea6d8bd6a5c212c0c9eca9fcbfb9c44
G2al <g2aluigi@gmail.com>
2026-01-19 18:41:30 +0100

    sistemato il ricerca

--- app/Filament/Resources/MovementResource/Pages/ListMovements.php
+++ app/Filament/Resources/MovementResource/Pages/ListMovements.php
@@ -6,6 +6,7 @@ use App\Filament\Resources\MovementResource;
 use App\Filament\Resources\MovementResource\Widgets\MovementStatsWidget;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListMovements extends ListRecords
 {
@@ -23,5 +24,35 @@ class ListMovements extends ListRecords
         return [
             MovementStatsWidget::class,
         ];
+    }
+
+    protected function applySearchToTableQuery(Builder $query): Builder
+    {
+        $this->applyColumnSearchesToTableQuery($query);
+
+        $search = $this->getTableSearch();
+
+        if (blank($search)) {
+            return $query;
+        }
+
+        foreach ($this->extractTableSearchWords($search) as $searchWord) {
+            $like = '%' . $searchWord . '%';
+
+            $query->where(function (Builder $query) use ($like) {
+                $query
+                    ->whereHas('vehicle', function (Builder $query) use ($like) {
+                        $query->where('plate', 'like', $like)
+                            ->orWhere('name', 'like', $like);
+                    })
+                    ->orWhereHas('station', fn (Builder $query) => $query->where('name', 'like', $like))
+                    ->orWhereHas('user', function (Builder $query) use ($like) {
+                        $query->where('name', 'like', $like)
+                            ->orWhere('surname', 'like', $like);
+                    });
+            });
+        }
+
+        return $query;
     }
 }
