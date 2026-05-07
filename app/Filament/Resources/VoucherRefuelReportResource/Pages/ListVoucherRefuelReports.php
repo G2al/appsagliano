d4eafce2f90ca02975a9ed1be602e79c30cbab38
G2al <g2aluigi@gmail.com>
2026-05-07 18:07:40 +0200

    Add report checkmarks to voucher refuels

--- app/Filament/Resources/VoucherRefuelReportResource/Pages/ListVoucherRefuelReports.php
+++ app/Filament/Resources/VoucherRefuelReportResource/Pages/ListVoucherRefuelReports.php
@@ -4,11 +4,28 @@ namespace App\Filament\Resources\VoucherRefuelReportResource\Pages;
 
 use App\Filament\Resources\VoucherRefuelReportResource;
 use App\Filament\Resources\VoucherRefuelReportResource\Widgets\VoucherRefuelStatsWidget;
+use App\Filament\Widgets\Concerns\InteractsWithReportTableChecks;
+use App\Models\Movement;
+use Carbon\Carbon;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Model;
 
 class ListVoucherRefuelReports extends ListRecords
 {
+    use InteractsWithReportTableChecks;
+
     protected static string $resource = VoucherRefuelReportResource::class;
+
+    public function table(Table $table): Table
+    {
+        $table = parent::table($table);
+
+        return $table->columns([
+            $this->getReportTableCheckColumn(),
+            ...$table->getColumnsLayout(),
+        ]);
+    }
 
     protected function getHeaderActions(): array
     {
@@ -21,5 +38,24 @@ class ListVoucherRefuelReports extends ListRecords
             VoucherRefuelStatsWidget::class,
         ];
     }
-}
 
+    protected function getReportTableRowKey(Model $record): string
+    {
+        /** @var Movement $record */
+        return 'movement:' . $record->getKey();
+    }
+
+    protected function getReportTableCheckDateRange(): array
+    {
+        $filters = $this->tableFilters ?? [];
+        $dateRange = $filters['date_range'] ?? [];
+
+        $startRaw = $dateRange['start'] ?? null;
+        $endRaw = $dateRange['end'] ?? null;
+
+        $start = Carbon::parse($startRaw ?: now()->startOfMonth())->startOfDay();
+        $end = Carbon::parse($endRaw ?: now())->endOfDay();
+
+        return [$start, $end];
+    }
+}
